284d5f5530999e8992d575c2ac93d91fc4c4eb77
a6228774 <a6228774@ulive.pccu.edu.tw>
2021-07-29 15:51:00 +0900

    Complete Logger

--- AccountingNote0729/Accounting.dbSource/Logger.cs
+++ AccountingNote0729/Accounting.dbSource/Logger.cs
@@ -10,6 +10,9 @@ namespace Accounting.dbSource
     {
         public static void Writelog (Exception ex)
         {
+            string msg = DateTime.Now.ToString("G") + ex.ToString();
+            System.IO.File.AppendAllText("D:\\Practice\\Webform0729\\Log.log" ,msg);
+
             throw ex;
         }
     }
